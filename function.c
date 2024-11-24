12e1ed2359f4944cf8872361a5131507860109d8
beambeambeam <supawitmarayat@gmail.com>
2024-11-24 20:10:34 +0700

    build: initial

---
+++ function.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void func1(void) { printf("Function 1!\n"); }
+
+void func2(void) { printf("Function 2!\n"); }
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "function.h"
+
+int main(void) {
+  func1();
+  func2();
+  return 0;
+}
